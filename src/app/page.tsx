73613cfcb8355a8d04078c466723e7a2719a788a
frustrated018 <niloyd018@gmail.com>
2024-02-13 23:50:58 +0600

    Moved the old components and styles to unused file for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,24 +10,6 @@ import {
 export default function Home() {
   return (
     <main className="min-h-screen flex justify-center items-center">
-      {/* <section className="h-auto lg:h-[50vh] w-[67%] bg-background rounded-lg border-2">
-        <h2 className="text-2xl lg:text-4xl text-center mt-5 font-semibold">
-          Unsplash URL Converter
-        </h2>
-        <div className="mt-2 text-sm md:text-base text-center text-muted-foreground">
-          Copy image url form{" "}
-          <a
-            href="https://unsplash.com"
-            target="_blank"
-            className="hover:text-primary underline"
-          >
-            unsplash.com
-          </a>{" "}
-          and paste it in the form below.
-        </div>
-        <SubmissionArea />
-      </section> */}
-
       <Card>
         <CardHeader>
           <CardTitle>Unsplash URL Converter</CardTitle>
---
+++ src/unused/unused.tsx
@@ -0,0 +1,65 @@
+//! Main page.tsx
+
+{
+  /* <section className="h-auto lg:h-[50vh] w-[67%] bg-background rounded-lg border-2">
+        <h2 className="text-2xl lg:text-4xl text-center mt-5 font-semibold">
+          Unsplash URL Converter
+        </h2>
+        <div className="mt-2 text-sm md:text-base text-center text-muted-foreground">
+          Copy image url form{" "}
+          <a
+            href="https://unsplash.com"
+            target="_blank"
+            className="hover:text-primary underline"
+          >
+            unsplash.com
+          </a>{" "}
+          and paste it in the form below.
+        </div>
+        <SubmissionArea />
+      </section> */
+}
+
+//! SubmissionArea.tsx
+
+{
+  /* <section className="my-5 space-y-5">
+  <div className="w-[90%] xl:w-3/4 mx-auto">
+    <Label>Copied URL:</Label>
+    <div className="flex flex-col md:flex-row gap-2">
+      <Input
+        type="url"
+        placeholder="Paste the url..."
+        value={originalUrl}
+        onChange={(e) => setOriginalUrl(e.target.value)}
+      />
+      <Button variant="outline" onClick={convertUrl}>
+        <UpdateIcon className="h-5 w-5" />
+      </Button>
+    </div>
+  </div>
+  <div className="w-[90%] xl:w-3/4 mx-auto">
+    <Label htmlFor="converted">Converted URL:</Label>
+    <div className="flex flex-col md:flex-row gap-2">
+      <Input
+        id="converted"
+        placeholder="Click to copy"
+        value={convertedUrl}
+        readOnly
+      />
+      <Button variant="outline" onClick={copyUrl}>
+        {copyClicked ? (
+          <CheckIcon className="h-5 w-5 text-green-500" />
+        ) : (
+          <CopyIcon className="h-5 w-5" />
+        )}
+      </Button>
+    </div>
+  </div>
+  <div className="grid place-items-center">
+    <Button variant="outline" onClick={performReset}>
+      Reset
+    </Button>
+  </div>
+</section>; */
+}
