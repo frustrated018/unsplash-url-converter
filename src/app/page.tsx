a57b92b67f45fe22cb4025d5af2a6d7a63bc6a98
frustrated018 <niloyd018@gmail.com>
2024-02-13 22:41:03 +0600

    Done with the basics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,19 @@ import { SubmissionArea } from "@/components/submissionArea";
 export default function Home() {
   return (
     <main className="h-screen flex justify-center items-center">
-      <section className="h-auto lg:h-[70vh] w-[70%] bg-background rounded-lg border-2">
-        <h2 className="text-4xl text-center mt-5">Unsplash URL Changer</h2>
+      <section className="h-auto lg:h-[50vh] w-[70%] bg-background rounded-lg border-2">
+        <h2 className="text-4xl text-center mt-5">Unsplash Link Changer</h2>
+        <div className="mt-2 text-center text-muted-foreground">
+          Copy Link form{" "}
+          <a
+            href="https://unsplash.com"
+            target="_blank"
+            className="hover:text-primary underline"
+          >
+            unsplash
+          </a>{" "}
+          and paste it
+        </div>
         <SubmissionArea />
       </section>
     </main>
--- src/components/submissionArea.tsx
+++ src/components/submissionArea.tsx
@@ -1,21 +1,65 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { CopyIcon, UpdateIcon } from "@radix-ui/react-icons";
+import { CheckIcon, CopyIcon, UpdateIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export function SubmissionArea() {
+  const [originalUrl, setOriginalUrl] = useState("");
+  const [convertedUrl, setConvertedUrl] = useState("");
+
+  const [copyClicked, setCopyClicked] = useState(false);
+
+  const convertUrl = () => {
+    // Conversion logic
+
+    const newUrl = originalUrl
+      .replace("unsplash.com", "source.unsplash.com")
+      .replace("photos/", "");
+
+    // console.log(newUrl);
+
+    //! Set the converted url
+    setConvertedUrl(newUrl);
+  };
+
+  const copyUrl = () => {
+    // Copy converted URL to clipboard
+    setCopyClicked(true);
+    navigator.clipboard.writeText(convertedUrl);
+  };
+
+  const performReset = () => {
+    setOriginalUrl("");
+    setConvertedUrl("");
+    setCopyClicked(false);
+  };
+
   return (
     <section className="mt-10 space-y-5">
       <div className="flex w-2/3 mx-auto space-x-2">
-        <Input type="url" placeholder="Paste the url..." />
-        <Button type="submit">
+        <Input
+          type="url"
+          placeholder="Paste the url..."
+          value={originalUrl}
+          onChange={(e) => setOriginalUrl(e.target.value)}
+        />
+        <Button variant="outline" onClick={convertUrl}>
           <UpdateIcon className="h-5 w-5" />
         </Button>
       </div>
       <div className="flex w-2/3 mx-auto space-x-2">
-        <Input placeholder="Converted Url" />
-        <Button>
+        <Input placeholder="Converted Url" value={convertedUrl} readOnly />
+        <Button variant="outline" onClick={copyUrl}>
+          {copyClicked ? (
+            <CheckIcon className="h-5 w-5 text-green-500" />
+          ) : (
             <CopyIcon className="h-5 w-5" />
+          )}
         </Button>
+      </div>
+      <div className="grid place-items-center">
+        <Button variant="outline" onClick={performReset}>Reset</Button>
       </div>
     </section>
   );
