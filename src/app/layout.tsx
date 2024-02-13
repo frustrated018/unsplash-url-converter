a35c1d8f1e55d64562b4c39c952e0e00a68f06e0
frustrated018 <niloyd018@gmail.com>
2024-02-13 23:14:58 +0600

    Changed fonts and updated title and subtitle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
-  title: "URL Changer",
-  description: "A simple tool for changing unsplash links",
+  title: "Unsplash URL Converter",
+  description:
+    "A simple tool for converting normal unsplash links to liks you can directly use as src in your next project",
 };
 
 export default function RootLayout({
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`}>{children}</body>
+      <body className={`${robotoMono.className} dark`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ export default function Home() {
   return (
     <main className="h-screen flex justify-center items-center">
       <section className="h-auto lg:h-[50vh] w-[67%] bg-background rounded-lg border-2">
-        <h2 className="text-2xl lg:text-4xl text-center mt-5">Unsplash Link Changer</h2>
+        <h2 className="text-2xl lg:text-4xl text-center mt-5 font-semibold">Unsplash URL Converter</h2>
         <div className="mt-2 text-sm md:text-base text-center text-muted-foreground">
-          Copy Link form{" "}
+          Copy image url form{" "}
           <a
             href="https://unsplash.com"
             target="_blank"
             className="hover:text-primary underline"
           >
-            unsplash
+            unsplash.com
           </a>{" "}
-          and paste it
+          and paste it in the form below.
         </div>
         <SubmissionArea />
       </section>
