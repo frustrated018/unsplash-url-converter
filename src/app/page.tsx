39dc256c2426718345c2de4177bf675cc349be60
frustrated018 <niloyd018@gmail.com>
2024-04-22 21:32:12 +0600

    Meteors from Aceternity ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Meteors } from "@/components/Meteors";
 import { SubmissionArea } from "@/components/submissionArea";
 import {
   Card,
@@ -9,27 +10,30 @@ import {
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-center items-center">
-      <Card>
-        <CardHeader>
-          <CardTitle>Unsplash URL Converter</CardTitle>
-          <CardDescription>
-            {" "}
-            Copy image url form{" "}
-            <a
-              href="https://unsplash.com"
-              target="_blank"
-              className="hover:text-primary underline"
-            >
-              unsplash.com
-            </a>{" "}
-            and paste it in the form below.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <SubmissionArea />
-        </CardContent>
-      </Card>
+    <main className="min-h-screen w-full">
+      <section className="absolute left-[30%] top-[30%] overflow-hidden">
+        <Card className="relative">
+          <CardHeader>
+            <CardTitle>Unsplash URL Converter</CardTitle>
+            <CardDescription>
+              {" "}
+              Copy image url form{" "}
+              <a
+                href="https://unsplash.com"
+                target="_blank"
+                className="hover:text-primary underline"
+              >
+                unsplash.com
+              </a>{" "}
+              and paste it in the form below.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <SubmissionArea />
+          </CardContent>
+        </Card>
+        <Meteors />
+      </section>
     </main>
   );
 }
